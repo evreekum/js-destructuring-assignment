8e2bb279db5d1b5e7cb86042390802533aa50bec
evreekum <e.v.reekum@gmail.com>
2022-06-06 13:49:44 +0200

    finished assignment

--- destructuring.js
+++ destructuring.js
@@ -6,6 +6,10 @@ const product = {
   screenType: 'LED',
 }
 
+const {price, refreshRate} = product;
+console.log(price);
+console.log(refreshRate);
+
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
 const productInformation = {
@@ -17,6 +21,11 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+
+const {type, brand} = productInformation.general;
+console.log(type);
+console.log(brand);
+
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -38,15 +47,29 @@ const tvOptions = {
   },
 };
 
+// const {wifi, bluetooth} = tvOptions.options.connectivity.popular;
+
+//OF:
+const {options:{connectivity:{popular:{wifi, bluetooth}}}} = tvOptions;
+
+console.log(wifi);
+console.log(bluetooth);
+
+
+
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
-
+const [first, second] = connectivity;
+console.log(first);
+console.log(second);
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
-
+const [fist, seconde, third, ...rest] = tvSizes;
+console.log(fist);
+console.log(third);
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
 
@@ -57,8 +80,18 @@ function getCompanyDetails() {
   }
 }
 
+const {name, adress} = getCompanyDetails();
+console.log(name);
+console.log(adress);
+
+
+
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+const [nameOne, ageSecond] = getDetails('Evelien', 32);
+console.log(nameOne);
+console.log(ageSecond);
